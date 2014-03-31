21d30445746426fae74f3bea8104180662c78142
Sonali <sonali.sharma@ischool.berkeley.edu>
2014-03-30 21:56:49 -0700

    Sonaliwebsite

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+
+
+console.log("Ready");
+
+$("#heading").on("click",function(){
+			console.log("Clcik");
+			window.location= "http://localhost:8888/simply-website/index.html";
+});
+
+
+});
